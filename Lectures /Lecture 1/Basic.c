c12b3bbe92be6c9a14e56e269d57cfd5d7c1beac
Mark <140330648+Sindisco@users.noreply.github.com>
2024-07-17 20:11:21 +0530

    Create Basic.c

---
+++ Lectures /Lecture 1/Basic.c
@@ -0,0 +1,127 @@
+//////////////////////////////////
+/////Lets make an int value//////
+////////////////////////////////
+
+
+#include<stdio.h>
+
+
+// int main(){
+//     printf("What is my name.");
+//     a = 6;
+//     printf(a);
+//     return 0;
+
+// Why are i getting lets see
+
+int main(){
+    printf("What is my name.");
+    int a;              //VArible declartion
+    a = 6;             // Varible intialization
+    printf("%d" ,a);   // % formet spicfier similar like (what type of varible we use)  "d" refer an int
+
+
+/////////////////// 
+//// Varibles ////   
+/////////////////
+
+/* a varinble is stroage location in memory that can hold different values during the execution of a program 
+
+in simply you say : varible stores the value of constant.
+
+for e.g. here a is a variable that stores the value 4*/
+
+
+// Tip: always write meaningfull varible names its help to make code more readable. //
+
+
+
+// Rule of variables
+
+/*
+1. first character is always be underscore or alphabhet.
+
+2. no comma ,blanks are allowed.
+
+3. No specials is allowed
+
+4. Varibles name are not same.*/
+
+
+// for e.g
+
+    int x;
+    int b;       // valide way
+    int d;
+
+    //int a
+    // int 7;
+    // int 747d;
+    // int -sd;     // invalide inputs
+    // int #sd;
+    // int earth is plant
+
+
+////////////////////
+/////Constant//////
+//////////////////
+
+
+/* In programming, a constant is a value that does not change during the execution of the program. For example, 4 is a constant because its value is fixed.*/ 
+
+
+//////////////////
+////Data types////
+//////////////////
+
+// there are few datatypes in c like int, char, float etc.
+
+    // int m;
+    // m = 45;
+    int m = 45;       //both are same
+
+    printf("%d",m);      
+    return 0 ;
+}
+
+
+
+//////////////////
+/////keywords/////
+//////////////////
+
+// There are some character that is reserve in c.
+
+// like : int , return etcs.
+
+
+//////////////////////////
+//// basic fuction of c///
+//////////////////////////
+
+// a C program starts with a main function and executes instructions persent insideee it.
+
+///rules////
+
+/*
+1. every progrems's execution start from main() fuction.
+
+2. Alll the staements are terminated with a semicolon.
+
+3. Intructions are case-senstivite.
+
+4. Intructions are excuted the same code in whuich they are writen.
+*/
+
+
+
+//////////////////
+/////Comments/////
+//////////////////
+
+// there are to types of comments in c
+
+// 1. Single line comments (//)
+// 2. Mutiline Comments (/**/)
+
+
